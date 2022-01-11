a4b4409c6475a80eef16d80179db7fdc773f6d00
Ramon Hitzeroth <ramon@luckybeard.com>
2022-01-11 08:50:08 +0200

    update env vars, add aws s3 provider

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'lb-cms-postgres-01.c0gyjn8zoehi.eu-west-1.rds.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'lb_cms_database_01'),
-      user: env('DATABASE_USERNAME', 'lbadmin'),
-      password: env('DATABASE_PASSWORD', '936I0LYZOIVf1CstBMHq'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
     },
   },
 });
